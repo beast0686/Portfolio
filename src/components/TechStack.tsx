e7a4f584ff2799af8b91fc30847c6f9140db0ff8
beast0686 <amansoudagar202@gmail.com>
2026-07-24 13:49:22 +0530

    bug: techstack overlap issue fix

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -177,6 +177,7 @@ const TechStack = () => {
         camera={{ position: [0, 0, 20], fov: 32.5, near: 1, far: 100 }}
         onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}
         className="tech-canvas"
+        style={{ position: "relative", zIndex: 0 }}
       >
         <ambientLight intensity={1} />
         <spotLight
