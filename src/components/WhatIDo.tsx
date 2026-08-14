1adbd7ad615e471792079a61ed242a4c9812eda8
beast0686 <amansoudagar202@gmail.com>
2026-08-14 17:52:48 +0530

    bug: What I Do text wrapper

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -29,9 +29,8 @@ const WhatIDo = () => {
       <div className="what-box">
         <h2 className="title">
           W<span className="hat-h2">HAT</span>
-          <div>
-            I<span className="do-h2"> DO</span>
-          </div>
+          <br />
+          I<span className="do-h2"> DO</span>
         </h2>
       </div>
       <div className="what-box">
